912d43ac048315d1850e2afab0f7a04c5d7b69d8
LLL <lil131@pitt.edu>
2020-10-28 03:53:08 -0400

    sb

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const links = [
-  { "name": "link-1", "url": "https://linkurl-1" },
-  { "name": "link-2", "url": "https://linkurl-2" },
-  { "name": "link-3", "url": "https://linkurl-3" },
+const LINKS = [
+  { "name": "cloudflare", "url": "https://www.cloudflare.com/" },
+  { "name": "github", "url": "https://www.github.com" },
+  { "name": "assignment", "url": "https://github.com/cloudflare-hiring/cloudflare-2020-general-engineering-assignment/#deploy-a-json-api" },
 ];
 
 const STATIC_LINK = 'https://static-links-page.signalnerve.workers.dev';
+const avatar_src = "https://avatars2.githubusercontent.com/u/43486346?s=400&u=720341aef70f93e7ef474f0be55c9e358868a75a&v=4";
+
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -13,55 +15,70 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  let response;
+  const link = new URL(request.url);
   try {
-    const link = new URL(request.url);
     if (link.pathname === '/links') {
-      response = new Response(JSON.stringify(links), { 
+      return new Response(JSON.stringify(LINKS), { 
         headers: { "content-type": "application/json" }
       });
     }  
     else {
-      response = await fetch(STATIC_LINK, {
-        headers: { "content-type": "text/html" }
-      });
+      const newLink = new URL('/links', link);
+      console.log(request.url)
+      return getStatic(newLink.hrel);
     }
   } catch {
-    response = new Response("ERROR", { status: 500 });
+    return new Response("ERROR", { status: 500 });
   }
-  return response;
 };
 
 
-// class LinksTransformer {
-//   constructor(links) {
-//     this.links = links;
-//   }
+class LinksTransformer {
+  constructor(links) {
+    this.links = links;
+  }
   
-//   // Target the div#links selector, and add in a new a for each link in your API using HTMLRewriter.
-//   async element(element) {
-//     console.log(element.tagName)
-//   }
-// }
+  //add in a new a for each link in your API using HTMLRewriter.
+  async element(element) {
+    this.links.forEach(link => {
+      element.append(`<a href=${link.url}>${link.name}</a>`, { html: Boolean });
+    });
+  }
+}
 
-// class HTMLHandler {
+class MakeVisible {
+  async element(element) {
+    element.removeAttribute('style');
+  }
+}
 
-//   element(element) {
-//     // An incoming element, such as `div`
-//     console.log(`Incoming element: ${element.tagName}`)
-//   }
+class ElementRewriter {
+  constructor(attributeName, value) {
+    this.attributeName = attributeName;
+    this.value = value;
+  };
 
-//   comments(comment) {
-//     // An incoming comment
-//   }
+  async element(element) {
+    element.setAttribute(this.attributeName, this.value);
+  };
+}
 
-//   text(text) {
-//     // An incoming piece of text
-//   }
-// }
+async function getStatic(link) {
+  const res = await fetch(STATIC_LINK, {
+    headers: { "content-type": "text/html" }
+    });
+  
+  let myLinks;
+  const response = await fetch(link, { 
+    headers: { "content-type": "application/json" }
+  });
+  myLists = JSON.stringify(await response.json());
 
-// async function handleRequest(req) {
-//   const res = await fetch(req);
-//   return res;
-//   // return new HTMLRewriter().on("div#links", new LinksTransformer()).transform(res);
-// }
+  // return res;
+  return new HTMLRewriter()
+    .on("div#links", new LinksTransformer(myLinks))
+    .on("div#profile", new MakeVisible())
+    .on("img#avatar", new ElementRewriter("src", avatar_src))
+    .on("h1#name", new ElementRewriter("innerText", "LLL"))
+    .transform(res);
+}
