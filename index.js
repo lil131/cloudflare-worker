c3cb94f7971aa69dc9583f9bce9b38a872ac14bf
LLL <lil131@pitt.edu>
2020-10-31 03:03:30 -0400

    finish

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const LINKS = [
   { "name": "cloudflare", "url": "https://www.cloudflare.com/" },
   { "name": "github", "url": "https://www.github.com" },
-  { "name": "assignment", "url": "https://github.com/cloudflare-hiring/cloudflare-2020-general-engineering-assignment/#deploy-a-json-api" },
+  { "name": "cloudflare assignment", "url": "https://github.com/cloudflare-hiring/cloudflare-2020-general-engineering-assignment/#deploy-a-json-api" },
 ];
 
 const STATIC_LINK = 'https://static-links-page.signalnerve.workers.dev';
-const avatar_src = "https://avatars2.githubusercontent.com/u/43486346?s=400&u=720341aef70f93e7ef474f0be55c9e358868a75a&v=4";
-// const SVG = '<svg class="svg-icon" viewBox="0 0 20 20"><path fill="none" d="M18.258,3.266c-0.693,0.405-1.46,0.698-2.277,0.857c-0.653-0.686-1.586-1.115-2.618-1.115c-1.98,0-3.586,1.581-3.586,3.53c0,0.276,0.031,0.545,0.092,0.805C6.888,7.195,4.245,5.79,2.476,3.654C2.167,4.176,1.99,4.781,1.99,5.429c0,1.224,0.633,2.305,1.596,2.938C2.999,8.349,2.445,8.19,1.961,7.925C1.96,7.94,1.96,7.954,1.96,7.97c0,1.71,1.237,3.138,2.877,3.462c-0.301,0.08-0.617,0.123-0.945,0.123c-0.23,0-0.456-0.021-0.674-0.062c0.456,1.402,1.781,2.422,3.35,2.451c-1.228,0.947-2.773,1.512-4.454,1.512c-0.291,0-0.575-0.016-0.855-0.049c1.588,1,3.473,1.586,5.498,1.586c6.598,0,10.205-5.379,10.205-10.045c0-0.153-0.003-0.305-0.01-0.456c0.7-0.499,1.308-1.12,1.789-1.827c-0.644,0.28-1.334,0.469-2.06,0.555C17.422,4.782,17.99,4.091,18.258,3.266"></path></svg>'
-const SVG = ' <svg xmlns="http://www.w3.org/2000/svg"> <text x="10" y="50" font-size="30">My SVG</text> </svg> '
+const avatar_src = "https://media-exp1.licdn.com/dms/image/C4E03AQFVThL5gSdlJA/profile-displayphoto-shrink_400_400/0?e=1609372800&v=beta&t=PV4etq9lHXANz2dtNLReKCwOTx0QG6wLKCkpBpvHXlM";
+const SVG_linkedin = '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn icon</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>';
+const SVG_github = '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub icon</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>';
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -20,12 +20,12 @@ async function handleRequest(request) {
   try {
     if (link.pathname === '/links') {
       return new Response(JSON.stringify(LINKS), { 
-        headers: { "Content-Type": "application/json" }
+        headers: { "Content-Type": "application/json;charset=UTF-8" }
       });
     }  
     else {
-      const newLink = new URL('/links', link);
-      return getStatic(newLink.href);
+      const jsonLink = new URL('/links', link);
+      return getStatic(jsonLink.href);
     }
   } catch(err) {
     console.log("error: ", err.message)
@@ -33,24 +33,35 @@ async function handleRequest(request) {
   }
 };
 
+async function getStatic() {
+  const res = await fetch(STATIC_LINK, {
+    headers: { "content-type": "text/html" }
+    });
 
-class LinksTransformer {
-  constructor(link) {
-    this.link = link;
-  }
-  
+  return new HTMLRewriter()
+    .on("div#links", new LinksHandler())
+    .on("div#profile", new AttrRemover("style"))
+    .on("img#avatar", new AttrRewriter("src", avatar_src))
+    .on("h1#name", new AttrRewriter("innerText", "LLL"))
+    .on("div#social", new AttrRemover("style"))
+    .on("div#social", new ContentAdder(`<a href="https://www.linkedin.com/in/linlu-liu-707b5b186/">${SVG_linkedin}</a>`))
+    .on("div#social", new ContentAdder(`<a href="https://github.com/lil131">${SVG_github}</a>`))
+    .on("title", new ContentAdder("Linlu Liu"))
+    .on("body", new AttrRewriter("style", "background-color : #73C3D5"))
+    .transform(res);
+}
+
+class LinksHandler {  
   //add in a new a for each link in your API using HTMLRewriter.
-  async element(element) {
-    // const myLinks = LINKS;
-    let myLinks;
-    const response = await fetch(this.link, { 
-      headers: { "Content-Type": "text/plain" }
-    });
-    myLists = JSON.parse(await response.json());
-
-    myLinks.forEach(link => {
-      element.append(`<a href=${link.url}>${link.name}</a>`, { html: Boolean });
-    });
+  element(element) {
+    try {
+      let myLinks = LINKS;
+      myLinks.forEach(link => {
+        element.append(`<a href=${link.url}>${link.name}</a>`, { html: Boolean });
+      });
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 }
 
@@ -59,7 +70,7 @@ class AttrRemover {
     this.attributeName = attributeName;
   }
 
-  async element(element) {
+  element(element) {
     element.removeAttribute('style');
   }
 }
@@ -70,35 +81,18 @@ class AttrRewriter {
     this.value = value;
   };
 
-  async element(element) {
+  element(element) {
     element.setAttribute(this.attributeName, this.value);
   };
 }
 
-class ContentRewriter {
+class ContentAdder {
   constructor(content) {
     this.content = content;
   }
 
-  async element(element) {
-    element.setInnerContent(this.content, { html: Boolean })
+  element(element) {
+    element.append(this.content, { html: Boolean })
   }
 }
 
-async function getStatic(link) {
-  const res = await fetch(STATIC_LINK, {
-    headers: { "content-type": "text/html" }
-    });
-
-  // return res;
-  return new HTMLRewriter()
-    .on("div#links", new LinksTransformer(LINKS))
-    .on("div#profile", new AttrRemover("style"))
-    .on("img#avatar", new AttrRewriter("src", avatar_src))
-    .on("h1#name", new AttrRewriter("innerText", "LLL"))
-    .on("div#social", new AttrRemover("style"))
-    .on("div#social", new ContentRewriter(`<a href="https://youtube.com">${SVG}</a>`))
-    .on("title", new ContentRewriter("Linlu Liu"))
-    .on("body", new AttrRewriter("style", "background-color : #73C3D5"))
-    .transform(res);
-}
